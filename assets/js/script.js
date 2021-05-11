193e4d7f649b2ff6f24bd2670679d8e5b946d834
RhysRichie <rhysrichardson@outlook.com>
2021-05-11 14:30:18 +0100

    Added calculation for mortality and recovery rate

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,17 @@ const updateTerm = () => {
                 const cData = data;
                 console.log(cData)
 
+                let recoverCalc = Math.ceil(data.All.recovered / data.All.confirmed * 100) + '%';
+                let deathCalc = Math.ceil(data.All.deaths / data.All.confirmed * 100) + '%';
+
                 const name = document.getElementById('country').innerHTML = data.All.country;
                 const population = document.getElementById('population').innerHTML = data.All.population;
                 const confirmed = document.getElementById('confirmed').innerHTML = data.All.confirmed;
                 const recovered = document.getElementById('recovered').innerHTML = data.All.recovered;
                 const deaths = document.getElementById('deaths').innerHTML = data.All.deaths;
+
+                const pRecovered = document.getElementById('recovery').innerHTML = recoverCalc;
+                const pdeath = document.getElementById('mortality').innerHTML = deathCalc;
             })
             .catch(error => console.log('request failed', error));
                 }
